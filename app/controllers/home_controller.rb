9c191497de053777e9adf67c8831218f3f9d467d
unknown <marija190396@gmail.com>
2020-07-04 23:05:23 +0200

    Show time entries recorded today on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
-    before_action :require_login
-    
-    def show
-        render :show
-    end
+  before_action :require_login
+  
+  def show
+    @time_entries_recorded_today = current_user.time_entries.recorded_today
+    render :show
+  end
 end

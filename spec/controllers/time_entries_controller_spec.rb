ce6482e3e0e08ac71af0e39187456480a5f1e19e
unknown <marija190396@gmail.com>
2020-07-05 20:26:29 +0200

    Test that time entries controller #create action returns bad request if user has active time entry

---
+++ spec/controllers/time_entries_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe TimeEntriesController, type: :request do
+  describe "#create" do
+    it "returns bad request if user has active time entry" do
+      user = create(:user)
+      time_entry = create(
+        :time_entry,
+        user: user,
+        start_time: Time.new(2020, 05, 04, 12, 00, 00),
+        end_time: nil
+      )
+
+      post time_entries_path(as: user),
+        params: { time_entry: { description: 'Test', start_time: '2020-05-04T19:55:10' } },
+        headers: { 'Accept' => 'application/javascript' }
+
+      expect(response.status).to eq 400
+    end
+  end
+end

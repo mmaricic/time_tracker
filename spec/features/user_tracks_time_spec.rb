46bb807955011ba080010ae57f5ab42027420c29
unknown <marija190396@gmail.com>
2020-07-06 23:41:59 +0200

    Test error when time entry overlaps with existing one

--- spec/features/user_tracks_time_spec.rb
+++ spec/features/user_tracks_time_spec.rb
@@ -1,32 +1,56 @@
 require "rails_helper"
 
 feature "user tracks time" do
-  scenario "using dashboard", js: true do
-    user = create(:user)
-    start_time = Time.new(2020, 5, 4, 10, 0, 0, Time.zone)
-    end_time = Time.new(2020, 5, 4, 13, 0, 0, Time.zone)
+  context "using dashboard", js: true do
+    scenario "successfully starts tracker" do
+      user = create(:user)
+      start_time = Time.new(2020, 5, 4, 10, 0, 0, Time.zone)
+      end_time = Time.new(2020, 5, 4, 13, 0, 0, Time.zone)
 
-    Timecop.freeze(start_time)
-    visit root_path(as: user) do
-      mock_browser_time
+      Timecop.freeze(start_time)
+      visit root_path(as: user) do
+        mock_browser_time
 
-      fill_in "time_entry_description", with: "Work"
-      click_button "start_timer_btn"
-    end
-  
-    Timecop.freeze(end_time)
-    visit root_path(as: user) do
-      mock_browser_time
-
-      click_button "stop_timer_btn"
-      
-      within "#time_entry_1" do
-        expect(page).to have_content("Work")
-        expect(page).to have_content("10:00")
-        expect(page).to have_content("13:00")
+        fill_in "time_entry_description", with: "Work"
+        click_button "start_timer_btn"
       end
+    
+      Timecop.freeze(end_time)
+      visit root_path(as: user) do
+        mock_browser_time
+
+        click_button "stop_timer_btn"
+        
+        within "#time_entry_1" do
+          expect(page).to have_content("Work")
+          expect(page).to have_content("10:00")
+          expect(page).to have_content("13:00")
+        end
+      end
+      Timecop.return
     end
-    Timecop.return
+    
+    scenario "fails because there is a time entry that includes current time" do
+      user = create(:user)
+      time_entry = create(
+        :time_entry,
+        user: user,
+        start_time: Time.new(2020, 5, 4, 8, 0, 0, Time.zone),
+        end_time: Time.new(2020, 5, 4, 11, 0, 0, Time.zone)
+      )
+      current_time = Time.new(2020, 5, 4, 10, 0, 0, Time.zone)
+      
+      Timecop.freeze(current_time)
+      visit root_path(as: user) do
+        mock_browser_time
+        accept_alert(/start time cannot overlap/i) do
+          click_button "start_timer_btn"
+        end
+        expect(page).not_to have_css("#stop_timer_btn")
+      end
+
+      Timecop.return
+    end
   end
 
   context "creating time entry manually" do

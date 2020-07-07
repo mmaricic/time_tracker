43fde41066b70f40336f2328008b0c0d453099d8
unknown <marija190396@gmail.com>
2020-07-07 13:54:14 +0200

    Implemenatation for adding total time to home page

--- app/helpers/time_entry_helper.rb
+++ app/helpers/time_entry_helper.rb
@@ -6,4 +6,9 @@ module TimeEntryHelper
 
     "#{hours}h #{mins}m #{secs}s"
   end
+
+  def pp_collection_total_time(time_entries)
+    total_time = time_entries.reduce(0) { |sum, time_entry| sum + time_entry.total_time }  
+    pp_total_time(total_time)
+  end
 end

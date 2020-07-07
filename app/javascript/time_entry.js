9b2f514f5717c6a6cb746c57130fc5fc6d858aec
unknown <marija190396@gmail.com>
2020-07-07 13:26:05 +0200

    Add time counter to home page

--- app/javascript/time_entry.js
+++ app/javascript/time_entry.js
@@ -1,6 +1,12 @@
-$(document).on('turbolinks:load', fillTimeFields);
+$(document).on('turbolinks:load', function() {
+  fillTimeFields();
+  startTimer();
+});
 
-$(document).on('ajax:success',  fillTimeFields);
+$(document).on('ajax:success', function() {
+  fillTimeFields();
+  startTimer();
+});
 
 function fillTimeFields() {
   $('#start_timer_form').submit(function() {
@@ -15,4 +21,34 @@ function fillTimeFields() {
 function getCurrentTime() {
   const offsetInMs = new Date().getTimezoneOffset() * 60000;
   return new Date(new Date() - offsetInMs).toISOString().substring(0, 19);
+}
+
+function startTimer() {
+  $('[data-timer]').each(function(index, element) {
+    const startTime = new Date(element.dataset.timer);
+   
+    const timerValue =  (new Date().getTime() - startTime.getTime()) / 1000;
+    element.innerHTML = formatTimerValue(timerValue);
+    setInterval(function() {
+      const timerValue =  (new Date().getTime() - startTime.getTime()) / 1000;
+      element.innerHTML = formatTimerValue(timerValue);
+    }, 1000)
+  })
+}
+
+function formatTimerValue(seconds) {
+  const secs = pad(Math.round(seconds % 60));
+  const mins = pad(Math.round(seconds / 60 % 60));
+  const hours = pad(Math.round(seconds / 3600));
+
+  return `${hours}:${mins}:${secs}`;
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
 }

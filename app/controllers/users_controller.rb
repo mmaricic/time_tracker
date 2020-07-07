92681749d6025fe934a7605f25a156d7174d48f0
unknown <marija190396@gmail.com>
2020-07-07 13:53:04 +0200

    Fix identation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < Clearance::UsersController
 
-    def create
-        @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-        if @user.save
-          sign_in @user
-          redirect_back_or root_url
-        else
-          render template: "users/new"
-        end
+    if @user.save
+      sign_in @user
+      redirect_back_or root_url
+    else
+      render template: "users/new"
     end
+  end
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:name, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end

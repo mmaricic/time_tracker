fb9102990996e649c0ffdff45d58897c7ef07d0f
unknown <marija190396@gmail.com>
2020-07-09 00:58:55 +0200

    Implement daily statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,5 +2,16 @@ class StatisticsController < ApplicationController
   before_action :require_login
 
   def show
+    @time_entries_recorded_on_date = current_user.time_entries.recorded_on_date(preview_date).order(:start_time)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def preview_date
+    @preview_date ||= params.key?(:preview_date) ? params[:preview_date] : Date.current
   end
 end

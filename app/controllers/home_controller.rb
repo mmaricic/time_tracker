38702a72f6b243344614847c6a421ef36490a284
unknown <marija190396@gmail.com>
2020-07-08 21:03:35 +0200

    Fixup order in list of time entries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_action :require_login
   
   def show
-    @time_entries_recorded_today = current_user.time_entries.recorded_today
+    @time_entries_recorded_today = current_user.time_entries.recorded_today.order(:start_time)
     if current_user.time_entries.active.exists?
       @active_time_entry = current_user.time_entries.active.first 
     end

ef40adc57e0f77a451f468d9aef2988a8cd0e504
unknown <marija190396@gmail.com>
2020-07-09 14:34:59 +0200

    Implement first name and initials methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
   has_many :time_entries, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 50}
+
+  def first_name
+    name.split(" ").first
+  end
+
+  def initials
+    words = name.split(" ")
+    if words.size > 1
+      (words.first[0] + words.second[0]).upcase
+    else
+      words.first[0, 2].upcase
+    end
+  end
 end

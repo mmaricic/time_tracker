488ca8fbbfe2c4b41832cc2cc1dd7b494a148075
unknown <marija190396@gmail.com>
2020-07-09 14:33:47 +0200

    Add tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,38 @@ describe User do
     expect(user.valid?).to be false
     expect(user.errors[:name]).not_to be_empty
   end
+
+  context "returns first name" do
+    it "when full name given" do
+      user = create(:user, name: "John Doe")
+
+      expect(user.first_name).to eq("John")
+    end
+
+    it "when only one word given" do
+      user = create(:user, name: "John")
+
+      expect(user.first_name).to eq("John")
+    end
+  end
+  
+  context "returns initials" do
+    it "when user provides first and last name" do
+      user = create(:user, name: "John Doe")
+
+      expect(user.initials).to eq("JD")
+    end
+
+    it "when user has middle name" do
+      user = create(:user, name: "John Unknown Doe")
+
+      expect(user.initials).to eq("JU")
+    end
+
+    it "when user provides only one word" do
+      user = create(:user, name: "Johnatan")
+
+      expect(user.initials).to eq("JO")
+    end   
+  end
 end
